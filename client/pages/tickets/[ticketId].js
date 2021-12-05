455ea06e43b98a22c49a0be8ae0d47563bf16fc5
azahrandani <azahra@srlabs.id>
2021-12-05 12:14:26 +0700

    Create new order from ticket details page

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,9 +1,38 @@
-const TicketShow = () => {
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = ({ ticket }) => {
+    const { doRequest, errors } = useRequest({
+        url: '/api/orders',
+        method: 'post',
+        body: {
+            ticketId: ticket.id
+        },
+        onSuccess: (order) => {
+            console.log('### succcess submitting new order', ticket.id);
+            console.log(order);
+        }
+    });
+
+    const orderTicket = async () => {
+        await doRequest();
+    };
+
     return (
         <div>
-            <h1>Ticket show</h1>
+            <h1>{ ticket.title }</h1>
+            <h3>Price: { ticket.price }</h3>
+            { errors }
+            <buton onClick={orderTicket} className="btn btn-primary">
+                Purchase
+            </buton>
         </div>
     );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+    const { ticketId } = context.query;
+    const { data } = await client.get(`/api/tickets/${ticketId}`);
+    return { ticket: data };
 };
 
 export default TicketShow;
