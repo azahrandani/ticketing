18a4fe0a0c1c5a1b7cff1a235e84b3c591251621
azahrandani <azahra@srlabs.id>
2021-12-05 12:34:45 +0700

    Show timer left in the order page

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,9 +1,35 @@
-const OrderShow = () => {
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+    const [timeLeft, setTimeLeft] = useState('');
+
+    // like on mount function
+    useEffect(() => {
+        const findTimeLeft = () => {
+            const milisecondsLeft = new Date(order.expiresAt) - new Date();
+            setTimeLeft(Math.round(milisecondsLeft / 1000));
+        };
+
+        findTimeLeft();
+        const timerId = setInterval(findTimeLeft, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        }
+    }, []);
+
     return (
         <div>
             <h1>Order Show</h1>
+            <p>{ timeLeft } seconds until order expires</p>
         </div>
     )
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+    const { orderId } = context.query;
+    const { data } = await client.get(`/api/orders/${orderId}`);
+    return { order: data };
 };
 
 export default OrderShow;
