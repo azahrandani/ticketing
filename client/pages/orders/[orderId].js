8a34d788421cb80862230380a8d9e13172488c5f
azahrandani <azahra@srlabs.id>
2021-12-05 13:01:25 +0700

    Make actual payment to our backend

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,20 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
     const [timeLeft, setTimeLeft] = useState(0);
+    const { doRequest, errors } = useRequest({
+        url: '/api/payments',
+        method: 'post',
+        body: {
+            orderId: order.id
+        },
+        onSuccess: (payment) => {
+            console.log('### succcess paying new order');
+            console.log(payment);
+        }
+    });
 
     // like on mount function
     useEffect(() => {
@@ -32,11 +44,12 @@ const OrderShow = ({ order, currentUser }) => {
             <h1>Please make your payment</h1>
             <p>{ timeLeft } seconds until order expires</p>
             <StripeCheckout
-                token={(token) => console.log(token)}
+                token={({ id }) => doRequest({ token: id })}
                 stripeKey="pk_test_51K317dFWfZOD1YVnROKElAij50u5V00QHz8zxmiwhhzZeh06MLLawXFMmwyZMUx1c1QFDXc4AcgON6Es2xqWuY9p00Ck8VrDZK"
                 amount={order.ticket.price * 100}
                 email={currentUser.email}
             />
+            { errors }
         </div>
     )
 };
