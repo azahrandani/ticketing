dc8271063e0168f788cbbc7f020dacbcde815ec6
azahrandani <azahra@srlabs.id>
2021-12-05 11:15:48 +0700

    Pass currentUser as a prop from the app wrapper to every component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,9 @@
-import buildClient from '../api/build-client';
-
 const Index = ({ currentUser }) => {
     return currentUser ? <h1>You are signed in!</h1> : <h1>You are not signed in :(</h1>;
 };
 
 Index.getInitialProps = async (context) => {
-    console.log('LANDING PAGE');
-    const client = buildClient(context);
-    const { data } = await client.get('/api/users/currentuser');
-    return data;
+    return {};
 };
 
 export default Index;
